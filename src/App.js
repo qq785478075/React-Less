6d8ce2be0846713fc9e73b32155ee6ff0addcd91
qq785478075 <785478075@qq.com>
2019-03-21 14:40:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import appReducer from './reducers/index.js';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
+
+const store = createStore(appReducer);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <Provider store={store}>
+        <div className="App" >
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
           </a>
-        </header>
-      </div>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import pageHeaderReducer from './pageHeader.js';
+
+const appReducer = combineReducers({
+    pageHeaderReducer
+});
+export default appReducer;
